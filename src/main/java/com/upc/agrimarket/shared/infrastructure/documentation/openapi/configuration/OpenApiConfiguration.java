51148101ac53a1598aae8f19e0a68752bee0ce61
Juan Angulo <juancarlosanguloabud@gmail.com>
2025-11-20 16:40:54 -0500

    feat: initial project configuration

---
+++ src/main/java/com/upc/agrimarket/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -0,0 +1,46 @@
+package com.upc.agrimarket.shared.infrastructure.documentation.openapi.configuration;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfiguration {
+    // Properties
+    @Value("${spring.application.name}")
+    String applicationName;
+
+    @Value("${documentation.application.description}")
+    String applicationDescription;
+
+    @Value("${documentation.application.version}")
+    String applicationVersion;
+
+    // Methods
+
+    @Bean
+    public OpenAPI learningPlatformOpenApi() {
+
+
+        // General configuration
+        var openApi = new OpenAPI();
+        openApi
+                .info(new Info()
+                        .title(this.applicationName)
+                        .description(this.applicationDescription)
+                        .version(this.applicationVersion)
+                        .license(new License().name("Apache 2.0")
+                                .url("https://springdoc.org")));
+
+
+        return openApi;
+    }
+}
---
+++ src/main/java/com/upc/agrimarket/shared/interfaces/rest/resources/MessageResource.java
@@ -0,0 +1,7 @@
+package com.upc.agrimarket.shared.interfaces.rest.resources;
+
+/**
+ * Message Resource
+ */
+public record MessageResource(String message) {
+}
